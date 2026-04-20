2415e23257a58e04dc698c142196e03e54057133
Gio <datunashvili72@gmail.com>
2026-04-20 18:55:49 +0400

    methods

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,16 +7,26 @@ public class Mover : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        PrintInstruction();
     }
 
     // Update is called once per frame
     void Update()
     {
+     MovePlayer();  
+    }
+
+    void PrintInstruction()
+    {
+        Debug.Log("Welcome!");
+    }
+
+    void MovePlayer()
+    {
         float x = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float y = 0f;
         float z = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
-    transform.Translate(x,y,z);
+        transform.Translate(x, y, z);
     }
 }
