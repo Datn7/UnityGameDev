c154708fb911da54c8b39755f9b50cebcfeaa4c0
Datn7 <datunashvili72@gmail.com>
2026-04-20 16:18:49 +0400

    basic setup

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float x;
+    public float y;
+    public float z;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(x,y,z);
+    }
+}
