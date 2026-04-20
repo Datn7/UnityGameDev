33fed4bc131a789893f9f9d3c7f5f1c426223c7e
Gio <datunashvili72@gmail.com>
2026-04-20 19:00:00 +0400

    mesh renderer

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -4,6 +4,9 @@ public class ObjectHit : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
+
+        GetComponent<MeshRenderer>().material.color = Color.white;
+
         Debug.Log("Someething Hit ME!");
     }
 }
